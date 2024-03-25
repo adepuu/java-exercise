4ff2e1adfc7509f90f7e4a8b6fd5bc2e8fe792f0
akaslayer <hendrysurijanto123@gmail.com>
2024-03-25 11:07:39 +0700

    lol

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,22 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        System.out.print("Enter number = ");
+        String numArr = myInput.nextLine();
+        String[] numArrInt = numArr.replaceAll("\\[","")
+                .replaceAll("]","").split(",");
+        int[] input = new int[numArrInt.length];
+        int i = 0;
+        for(String num : numArrInt){
+            input[i] =Integer.valueOf(num);
+            i++;
+        }
+        Arrays.sort(input);
+        System.out.print("Output = " + input[input.length-1]);
+    }
 }
