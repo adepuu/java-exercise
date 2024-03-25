a6ef51e1e62495046606bb2559340392f6c5a478
nurrizkiamalia <rizkiamel9@gmail.com>
2024-03-25 14:19:48 +0700

    feat: remove every number in list by input

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,5 +1,10 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise6 {
     /**
      * Java Array Program to Remove All Occurrences of an Element in an Array
@@ -12,6 +17,24 @@ public class Exercise6 {
      * Final result: [2, 3, 5]
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Input n: ");
+        int n = input.nextInt();
+        int removeValue;
+        List<Integer> array = new ArrayList<>();
+
+        System.out.println("Input array list: ");
+        for (int i = 0; i < n; i++){
+            array.add(input.nextInt());
+        }
+
+        System.out.println("Number to remove: ");
+        removeValue = input.nextInt();
+        if (array.contains(removeValue)){
+            array.removeAll(Collections.singleton(removeValue));
+        }
+
+        System.out.println("result: "+array);
     }
 }
