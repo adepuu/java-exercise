bddfe719be511b9c2b517e80d65ed3e952eacf97
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 09:29:36 +0700

    feat(exercise1):Exercise number 1 done

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,13 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        int[] allNumber = {0, 7, 2, 5, 1, 4};
+        int highestNumber = 0;
+        for (int i : allNumber) {
+            if(highestNumber <= i)
+                highestNumber = i;
+        }
+        System.out.println("The highest number is : "+highestNumber);
+    }
 }
