e3b785c2620d387f924f517819a84efaf3d953bd
Rizky Ramadhan <ramarizdev@gmail.com>
2024-03-25 11:48:52 +0700

    feat(exercises): add exercise6 solution

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise6 {
     /**
      * Java Array Program to Remove All Occurrences of an Element in an Array
@@ -11,7 +15,19 @@ public class Exercise6 {
      * [ 1, 2, 1, 3, 5, 1 ]  to
      * Final result: [2, 3, 5]
      */
-    public static void main(String[] args) {
+    static ArrayList<Integer> removeAllOccurrences(Integer[] arr, int key) {
+        ArrayList<Integer> result = new ArrayList<>();
 
+        for(int item: arr) {
+            if(item != key) {
+                result.add(item);
+            }
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 1, 3, 5, 1};
+        System.out.println(removeAllOccurrences(nums,1));
     }
 }
