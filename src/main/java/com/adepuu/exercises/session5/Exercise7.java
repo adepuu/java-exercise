f410cfd8310522a6def7957ba0baa19d74474f88
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-25 15:59:21 +0700

    feat(exercise7) done exercise 7

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,18 +1,26 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise7 {
-    /**
-     * Java String Program to Check Anagram
-     *
-     * Example 1:
-     * Input: s = "anagram", t = "nagaram"
-     * Output: true
-     *
-     * Example 2:
-     * Input: s = "rat", t = "car"
-     * Output: false
-     */
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise7 {
+    public static void main(String[] args) {
+        String word1 = "anagram";
+        String word2 = "nagaram";
+
+        System.out.println("Input : " + word1 + " AND " + word2);
+        boolean result = isAnagram(word1, word2);
+        System.out.println("Output : " + result);
+    }
+
+    public static boolean isAnagram(String w1, String w2){
+        if (w1.length() != w2.length()){
+            return false;
+        }
+        char[] w1Char = w1.toCharArray();
+        char[] w2Char = w2.toCharArray();
+        Arrays.sort(w1Char);
+        Arrays.sort(w2Char);
+
+        return Arrays.equals(w1Char, w2Char);
     }
 }
