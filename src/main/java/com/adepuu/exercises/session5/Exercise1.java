09e738a127eb751ecd181bf40d737c60844a270e
Christofer Bryan <christoferbryan45@gmail.com>
2024-03-25 12:11:04 +0800

    feat(exercise): completed exercise 1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,28 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> inputList = new ArrayList<>();
+        inputList.add(25);
+        inputList.add(40);
+        inputList.add(76);
+        inputList.add(39);
+        inputList.add(81);
+
+        System.out.println(maxValue(inputList));
+    }
+
+    public static Integer maxValue(ArrayList<Integer> inputArray){
+        Integer max = Integer.MIN_VALUE;
+
+        for(Integer num : inputArray){
+            if(num>max){
+                max = num;
+            }
+        }
+
+        return max;
+    }
 }
