851e60eb65c8f5f9f50d9c8799034be62a2678be
akaslayer <hendrysurijanto123@gmail.com>
2024-03-25 11:32:24 +0700

    Exrcs4(done) Exercise 4 for remove duplicate elements from an array is done

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,10 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +14,18 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
-
+        Scanner myInput = new Scanner(System.in);
+        System.out.print("Enter number = ");
+        String numArr = myInput.nextLine();
+        String[] numArrInt = numArr.replaceAll("\\[","")
+                .replaceAll("]","").split(",");
+        List<Integer> intList = new ArrayList<>();
+        int i = 0;
+        for(String num : numArrInt){
+            if(!intList.contains(Integer.valueOf(num))){
+                intList.add(Integer.valueOf(num));
+            }
+        }
+        System.out.println("Output = " + intList);
     }
 }
