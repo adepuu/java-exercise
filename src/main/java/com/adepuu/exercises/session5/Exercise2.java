3bf4db951f1f52635bbe9b51c3959ea595e00693
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 09:45:18 +0700

    feat(exercise2):Completing Exercise 2

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +11,22 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
-
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+        arr.add(6);
+        arr.add(7);
+        int d = 2;
+        for(int i = 0; i < d; i++){
+            int tempNum = arr.get(i);
+            arr.add(tempNum);
+        }
+        for(int i = 0; i < d; i++){
+            arr.remove(0);
+        }
+        System.out.println(arr.toString());
     }
 }
