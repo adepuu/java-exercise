fe93080c865a9cf29b17baeb819685fabbc31890
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 10:08:32 +0700

    feat(Exercise 1) : Use Scanner for Array

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,8 +1,24 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercise1 {
     public static void main(String[] args) {
-        int[] allNumber = {0, 7, 2, 5, 1, 4};
+        Scanner numInput = new Scanner(System.in);
+        ArrayList<Integer> allNumber = new ArrayList<Integer>();
+        boolean isDone = false;
+        
+        do {
+            System.out.print("Input a number : ");
+            allNumber.add(numInput.nextInt());
+            System.out.print("Input again? (press y to continue) ");
+            numInput.nextLine();
+            if(!numInput.nextLine().equals("y"))
+            isDone = true;
+        } while (!isDone);
+        numInput.close();
+
         int highestNumber = 0;
         for (int i : allNumber) {
             if(highestNumber <= i)
