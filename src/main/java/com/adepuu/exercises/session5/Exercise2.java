6dfe702bdda5beb968b0e0d37c76f28eff376091
Christofer Bryan <christoferbryan45@gmail.com>
2024-03-25 16:15:14 +0800

    feat(exercise): completed exercise 2

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +11,20 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(rotateArray(arr, 2)));
+    }
 
+    public static int[] rotateArray(int[] inputArray, int d) {
+
+        for (int i = 0; i < d; i++) {
+            int last = inputArray[0];
+            for (int j = 0; j < inputArray.length - 1; j++) {
+                inputArray[j] = inputArray[j + 1];
+            }
+            inputArray[inputArray.length - 1] = last;
+        }
+
+        return inputArray;
     }
 }
