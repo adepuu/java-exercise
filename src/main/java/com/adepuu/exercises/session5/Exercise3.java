28c1566228d1306f9d7c1b63d612301bac2bf9ec
alnandoh <alnandoharsono@gmail.com>
2024-03-25 10:24:48 +0700

    Finish exercise 3

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -17,6 +17,17 @@ public class Exercise3 {
      * Output: true
      */
     public static void main(String[] args) {
+        int[] input = {1,2,3,1};
+        System.out.println(containDuplicate(input));
+    }
 
+    public static boolean containDuplicate(int[] input){
+        for (int i = 0; i < input.length-1; i++) {
+            for (int j = i+1; j < input.length; j++) {
+                if(input[i]==input[j]){
+                    return true;
+                }
+            }
+        }return false;
     }
 }
