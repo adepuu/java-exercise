55f9ef2cc41dac5338fea0913a7bf8b62472a489
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-25 10:38:13 +0700

    feat(exercise1) done exercise 1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,20 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        int[] number = {7, 2, 5, 1, 4};
+        int largestNumber = findLargest(number);
+        System.out.println("The largest number in the array is : " + largestNumber);
+    }
+
+    public static int findLargest(int[] number) {
+        int largestNumber = number[0];
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] > largestNumber) {
+                largestNumber = number[i];
+            }
+        }
+        return largestNumber;
+    }
 }
+
