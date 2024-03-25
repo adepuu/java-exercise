983837d0abbd0eec8bbfe3e1abb0a026c965c687
alnandoh <alnandoharsono@gmail.com>
2024-03-25 14:49:25 +0700

    Finished exercise 9

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -19,13 +19,28 @@ public class Exercise9 {
      * Output: [1,1,0]
      */
     public static void main(String[] args) {
-//        int[] input={73,74,75,71,69,72,76,73};
-//        var result = warmerTemperature(input);
-//        System.out.println(result);
-//    }
-//
-//    public static ArrayList<Integer> warmerTemperature(int[] input){
-//        ArrayList<Integer> result = new ArrayList<>();
-//
+        int[] input={73,74,75,71,69,72,76,73};
+        var result = warmerTemperature(input);
+        System.out.println(result);
+    }
+
+    public static ArrayList<Integer> warmerTemperature(int[] input){
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < input.length-1; i++) {
+            boolean foundWarmer=false;
+
+            for (int j = i+1; j < input.length; j++) {
+                if (input[j] > input[i]) {
+                    result.add(j - i);
+                    foundWarmer = true;
+                    break;
+                }
+            }
+            if (!foundWarmer){
+                result.add(0);
+            }
+        }
+        result.add(0);
+        return result;
     }
 }
