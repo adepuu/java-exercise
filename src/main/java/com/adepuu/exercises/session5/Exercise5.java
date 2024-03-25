5f2d83c023e71cd64dd0b7d9f9f0952971a9216c
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 11:13:41 +0700

    feat(Exercise5) Completing Exercise 5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -13,6 +17,33 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        ArrayList<Integer> numberArray = new ArrayList<Integer>();
+        ScanArray inputNumber = new ScanArray();
+        Scanner myScanner = new Scanner(System.in);
 
+        numberArray = inputNumber.inputArray();
+        String direction = "";
+        boolean flag = true;
+        
+        System.out.print("Do you want to sort Ascending or Descending? (asc or desc) : ");
+        do {
+            direction = myScanner.nextLine();
+            if(direction.toLowerCase().equals("asc") || direction.toLowerCase().equals("desc")){
+                flag = true;
+            }
+            else{
+                System.out.print("Please input only (asc or desc) : ");
+                flag = false;
+            }
+        } while (!flag);
+        
+        myScanner.close();
+
+        if(direction.toLowerCase().equals("asc")){
+            Collections.sort(numberArray);  
+        } else {
+            Collections.sort(numberArray, Collections.reverseOrder());  
+        }
+        System.out.println("This is your "+direction.toUpperCase()+ " numbers : " + numberArray.toString());
     }
 }
