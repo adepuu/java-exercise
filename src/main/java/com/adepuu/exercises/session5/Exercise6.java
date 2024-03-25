e1bab46dae766e66dc623ce84143516a06e1cff2
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-25 22:56:07 +0700

    feat(exercise6) done exercise 6

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,17 +1,33 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise6 {
-    /**
-     * Java Array Program to Remove All Occurrences of an Element in an Array
-     *
-     * Input: array = [ 1, 2, 1, 3, 5, 1 ] , key = 1
-     * Output: [2, 3, 5]
-     *
-     * Explanation: all the occurrences of element 1 is removed from the array So, array becomes from
-     * [ 1, 2, 1, 3, 5, 1 ]  to
-     * Final result: [2, 3, 5]
-     */
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise6 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 1, 3, 5, 1};
+        int key = 1;
+
+        System.out.println("Input Array : " + Arrays.toString(array));
+        int[] result = removeArray(array, key);
+        System.out.println("Removed Array : " + Arrays.toString(result));
     }
+    public static int[] removeArray(int[] array, int key){
+        int count = 0;
+
+        for (int number : array) {
+            if (number != key){
+                count++;
+            }
+        }
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int number : array){
+            if (number != key){
+                result[index++] = number;
+            }
+        }
+        return result;
+    }
+
 }
