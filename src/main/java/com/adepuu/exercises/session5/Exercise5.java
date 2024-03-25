9fc7c56f1134258baef5a185a36cbfa6e17eb7ef
alnandoh <alnandoharsono@gmail.com>
2024-03-25 15:36:37 +0700

    Finished exercise 5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,8 +1,51 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise5 {
 
     public static void main(String[] args) {
+        int[] input = {8, 7, 5, 2};
+        var resultASC=sortASC(input);
+        var resultDSC=sortDSC(input);
+        System.out.println(resultASC);
+        System.out.println(resultDSC);
+    }
 
+    public static ArrayList<Integer> sortASC(int[] input){
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int num : input) {
+            result.add(num);
+        }
+
+        for (int i = 0; i < result.size() - 1; i++) {
+            for (int j = 0; j < result.size() - i - 1; j++) {
+                if (result.get(j) > result.get(j + 1)) {
+                    int temp = result.get(j);
+                    result.set(j, result.get(j + 1));
+                    result.set(j + 1, temp);
+                }
+            }
+        }
+        return result;
+    }
+    public static ArrayList<Integer> sortDSC(int[] input){
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int num : input) {
+            result.add(num);
+        }
+
+        for (int i = 0; i < result.size() - 1; i++) {
+            for (int j = 0; j < result.size() - i - 1; j++) {
+                if (result.get(j) < result.get(j + 1)) {
+                    int temp = result.get(j);
+                    result.set(j, result.get(j + 1));
+                    result.set(j + 1, temp);
+                }
+            }
+        }
+        return result;
     }
 }
