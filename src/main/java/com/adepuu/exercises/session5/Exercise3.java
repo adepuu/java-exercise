04f21aa7bb3623a0a7650b103e08f1cdb91f2138
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-25 15:57:35 +0700

    feat(exercise3) done exercise 3

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -1,22 +1,24 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise3 {
-    /**
-     * Write a Java Program to Check if Array Contain Duplicates
-     *
-     * Example 1:
-     * Input: nums = [1,2,3,1]
-     * Output: true
-     *
-     * Example 2:
-     * Input: nums = [1,2,3,4]
-     * Output: false
-     *
-     * Example 3:
-     * Input: nums = [1,1,1,3,3,4,3,2,4,2]
-     * Output: true
-     */
     public static void main(String[] args) {
+        int[] number1 = {1, 2, 3, 1};
+        int[] number2 = {1, 2, 3, 4};
+        int[] number3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
 
+        System.out.println("Example 1 = 1, 2, 3, 1 are : " + checkDuplicate(number1));
+        System.out.println("Example 2 = 1, 2, 3, 4 are : " + checkDuplicate(number2));
+        System.out.println("Example 3 = 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 are : " + checkDuplicate(number3));
+    }
+
+    public static boolean checkDuplicate(int[] number){
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                if (number[i] == number[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
