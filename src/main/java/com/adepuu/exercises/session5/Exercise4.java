8df7fe6b55d93bad43a584f8cfa36f9f6460f6fb
alnandoh <alnandoharsono@gmail.com>
2024-03-25 11:04:03 +0700

    Finish exercise 4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +12,21 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        int[] input={1, 2, 2, 3, 3, 3, 4, 5};
+        var result = removeDuplicates(input);
+        System.out.println(result);
+    }
 
+    public static ArrayList<Integer> removeDuplicates(int[] input){
+        ArrayList<Integer> result = new ArrayList<>();
+        if (input.length > 0) {
+            result.add(input[0]);
+        }
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] != input[i - 1]) {
+                result.add(input[i]);
+            }
+        }
+        return result;
     }
 }
