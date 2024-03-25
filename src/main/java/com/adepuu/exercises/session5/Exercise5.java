95a67b267dd471b54959d9a0e0bb2248a8ac3e68
nurrizkiamalia <rizkiamel9@gmail.com>
2024-03-25 13:28:33 +0700

    feat: Input number to sort for Ascending and Descending

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Scanner;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -13,6 +15,23 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int n = 4;
+        int[] array = new int[n];
+
+        System.out.println("Input Array number to Ascending: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = input.nextInt();
+            System.out.print(array[i]+" ");
+        }
+
+        System.out.println();
+
+        System.out.println("Input Array number to Descending: ");
+        for (int i = 0; i < n; i++)
+            array[i] = input.nextInt();
+            for (int j = n - 1; j >= 0; j--)
+                System.out.print(array[j]+" ");
     }
 }
