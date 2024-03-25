5ea7d7a7b74a92ee6f58a3f5f78abfe4059f5d84
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 14:01:20 +0700

    feat(Exercise 9) : Completing Exercise 9

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise9 {
     /**
      * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
@@ -17,6 +19,23 @@ public class Exercise9 {
      * Output: [1,1,0]
      */
     public static void main(String[] args) {
+        ArrayList<Integer> temperatures = ScanArray.inputArray();
+        ArrayList<Integer> dayToWait = new ArrayList<Integer>();
 
+        for(int i = 0; i < temperatures.size(); i++){
+            int dayCount = 0;
+            try {
+                for(int j = i+1; j <= temperatures.size(); j++){
+                    dayCount += 1;
+                    if(temperatures.get(i)<temperatures.get(j)){
+                        dayToWait.add(dayCount);
+                        break;
+                    }
+                }    
+            } catch (Exception e) {
+                dayToWait.add(0);
+            }
+        }
+        System.out.println(dayToWait.toString());
     }
 }
