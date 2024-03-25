db7c47668d03d7289caab5d756feca80093820ca
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 11:20:05 +0700

    feat(Exercise 6) : Completing exercise 6

--- src/main/java/com/adepuu/exercises/session5/Exercise6.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise6.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Exercise6 {
     /**
      * Java Array Program to Remove All Occurrences of an Element in an Array
@@ -12,6 +15,21 @@ public class Exercise6 {
      * Final result: [2, 3, 5]
      */
     public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        ScanArray inputNumber = new ScanArray();
+        Scanner myScanner = new Scanner(System.in);
+        nums = inputNumber.inputArray();
 
+        System.out.print("What number do you want to remove? : ");
+        int removedNum = myScanner.nextInt();
+        myScanner.close();
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums.get(i)==removedNum){
+                nums.remove(i);
+                i--;
+            }
+        }
+        System.out.println("This is your number without "+ removedNum +" : "+nums.toString());
     }
 }
