7cefcbb021bf6bbad12c70aa2ad78a718ad7e52b
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 10:01:53 +0700

    feat(exercise4) : Completing exercise 4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +11,24 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(2);
+        nums.add(3);
+        nums.add(3);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
 
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i+1; j < nums.size(); j++){
+                if(nums.get(i)==nums.get(j)){
+                    nums.remove(j);
+                    j--;
+                }
+            }
+        }
+        System.out.println(nums.toString());
     }
 }
