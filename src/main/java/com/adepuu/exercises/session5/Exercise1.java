3890bb7504bd37c7786506e0e96368a060040a95
Rizal Solihin <ripisopol@gmail.com>
2024-03-25 10:28:43 +0700

    Add largest element finding function, Exercise1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,20 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static int largest(int[] array) {
+        int largestArray = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largestArray) {
+                largestArray = array[i];
+            }
+        }
+        return largestArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {7, 2, 5, 1, 4};
+        int largestElement = largest(array);
+        System.out.println("Largest element in the array is: " + largestElement);
+    }
 }
