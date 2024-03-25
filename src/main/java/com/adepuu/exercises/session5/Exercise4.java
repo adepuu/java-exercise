de726dfb6b19d882355f204f2f332d62223409dc
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-25 14:38:29 +0700

    feat(exercise 4) done exercise 4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,14 +1,33 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise4 {
-    /**
-     * Java Array Program to Remove Duplicate Elements From an Array
-     *
-     * Input: [ 1, 2, 2, 3, 3, 3, 4, 5 ]
-     * Output: [ 1, 2, 3, 4, 5 ]
-     * @param args
-     */
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise4 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 3, 3, 4, 5};
+        int[] result = removeDuplicate(array);
+        System.out.println("Input : " + Arrays.toString(array));
+        System.out.println("Output : " + Arrays.toString(result));
+    }
+    public static int[] removeDuplicate(int[] array){
+        Arrays.sort(array);
+
+        int uniqueNum = 1;
+        for (int i = 1; i < array.length ; i++) {
+            if (array[i] != array[i - 1]){
+                uniqueNum++;
+            }
+        }
+
+        int[] result = new int [uniqueNum];
+        result[0] = array[0];
+
+        int index = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1]){
+                result[index++] = array[i];
+            }
+        }
+        return result;
     }
 }
