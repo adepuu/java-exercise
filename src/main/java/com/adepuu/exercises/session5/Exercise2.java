dc3a4b7ffcb6d24559c708418b48ac86d062baa0
Rizky Ramadhan <ramarizdev@gmail.com>
2024-03-25 10:18:10 +0700

    feat: add exercise2 solution

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +13,20 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int d = 2;
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> rotated = new ArrayList<>();
 
+        for(int i = 0; i < arr.length; i++) {
+            if(i < d) {
+                rotated.add(arr[i]);
+            } else {
+                result.add(arr[i]);
+            }
+        }
+        result.addAll(rotated);
+
+        System.out.println(result);
     }
 }
