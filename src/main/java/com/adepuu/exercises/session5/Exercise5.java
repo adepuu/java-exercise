8d43edb520e2a6cb2faf95f4e7d7672a145067dd
Yosef1997 <yosefsitumorang97@gmail.com>
2024-03-25 16:32:01 +0700

    feat(exercise5 & exercise9): done

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -12,7 +14,53 @@ public class Exercise5 {
      * Input: [ 8, 7, 5, 2], direction = "desc"
      * Output: [ 8,7, 5, 2 ]
      */
-    public static void main(String[] args) {
 
+   public static void sortAsc()
+    {
+        int[] arr = {8, 7, 5, 2};
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++)
+        {
+            int min_idx = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                  min_idx = j;
+                }
+            }
+
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+
+      System.out.println("result = " + Arrays.toString(arr));
+    }
+
+  public static void sortDesc()
+  {
+    int[] arr = {8, 6, 7, 5, 2};
+    int n = arr.length;
+
+    for (int i = 0; i < n-1; i++)
+    {
+      int max_idx = i;
+      for (int j = i+1; j < n; j++) {
+        if (arr[j] > arr[max_idx]) {
+          max_idx = j;
+        }
+      }
+
+      int temp = arr[max_idx];
+      arr[max_idx] = arr[i];
+      arr[i] = temp;
+    }
+
+    System.out.println("result = " + Arrays.toString(arr));
+  }
+
+    public static void main(String[] args) {
+        sortAsc();
+        sortDesc();
     }
 }
--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Exercise9 {
     /**
      * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
@@ -16,7 +19,33 @@ public class Exercise9 {
      * Input: temperatures = [30,60,90]
      * Output: [1,1,0]
      */
-    public static void main(String[] args) {
 
+    public static void checkTemperature () {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(73,74,75,71,69,72,76,73));
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.size()-1; ++i){
+          boolean isSwapped =false;
+
+            for (int j = i + 1; j < arr.size(); ++j) {
+              if(arr.get(j)> arr.get(i)){
+                  result.add(j-i);
+                  isSwapped = true;
+                  break;
+              }
+          }
+          if(!isSwapped){
+              result.add(0);
+          }
+        }
+        result.add(0);
+
+
+        System.out.println("result = " + result);
+    }
+
+    public static void main(String[] args) {
+        checkTemperature();
     }
 }
