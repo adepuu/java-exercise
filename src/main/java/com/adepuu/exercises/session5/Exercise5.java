3fba1ee71066fe8f37decf3ae770d6f045c8cc8c
akaslayer <hendrysurijanto123@gmail.com>
2024-03-25 11:58:37 +0700

    Exrcs5(done): Exercise 5 for sorting array in increasing & decreasing order is done

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.*;
+
 public class Exercise5 {
     /**
      * Java Array Program to sort array in increasing & decreasing order
@@ -13,6 +15,36 @@ public class Exercise5 {
      * Output: [ 8,7, 5, 2 ]
      */
     public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        boolean loop = false;
+        List<Integer> numArr = new ArrayList<>();
 
+        while(!loop){
+            System.out.print("Input Number = ");
+            int input = myInput.nextInt();
+            numArr.add(input);
+            System.out.print("Continue (y) / Stop (n) = ");
+            char choice = myInput.next().charAt(0);
+            switch (choice){
+                case 'y':break;
+                case 'n':loop = true;
+                         break;
+
+            }
+        }
+        System.out.println();
+        myInput.nextLine();
+        System.out.print("Direction (asc) / (desc) = ");
+        String directionChoice = myInput.nextLine();
+        switch (directionChoice){
+            case "asc" : Collections.sort(numArr);
+                        System.out.print("Output = " + numArr);
+                         break;
+
+            case "desc" :Collections.sort(numArr,Collections.reverseOrder());
+                         System.out.print("Output = " + numArr );
+                         break;
+
+        }
     }
 }
