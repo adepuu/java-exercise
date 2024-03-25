8e676f2602dc5c68baaa903df5e5f8e27d31eeb6
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 09:53:22 +0700

    feat(exercise3) : Completing Exercise 3

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -17,6 +17,20 @@ public class Exercise3 {
      * Output: true
      */
     public static void main(String[] args) {
-
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        boolean flag = false;
+        for(int i = 0; i < nums.length; i++){
+            if(!flag){
+                for(int j = i+1; j < nums.length; j++){
+                    if(nums[i]==nums[j]){
+                        flag = true;
+                        System.out.println(flag);
+                        break;
+                    }
+                }
+            }else{
+                break;
+            }
+        }
     }
 }
