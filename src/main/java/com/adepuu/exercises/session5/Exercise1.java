0ae9f5e032a0b92818899a10896f5588279d6f6d
Yosef1997 <yosefsitumorang97@gmail.com>
2024-03-25 10:24:43 +0700

    feat(exercise1): findLargest value in array with one loop

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,22 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise1 {
+//    Write a Java Program to Find the Largest Element in Array
+//
+//    Input: [ 7, 2, 5, 1, 4]
+//    Output: 7
+    public static void findLargest () {
+        int[] arr= {7, 2, 5, 1, 4};
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i = 0; i < (arr.length - 1); ++i){
+            if(arr[i]> arr[i + 1]){
+                result.add(arr[i]);
+            }
+            System.out.println(result);
+            return;
+        }
+    }
 }
