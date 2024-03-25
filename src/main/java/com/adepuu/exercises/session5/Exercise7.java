3f0de4d8bf8b86b06e7281ef497ff1973e1c9066
Rinaldes Duma <rinaldes.duma@gmail.com>
2024-03-25 12:05:06 +0700

    feat(exercise 7) : Exercise 7 Completed

--- src/main/java/com/adepuu/exercises/session5/Exercise7.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise7.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercise7 {
     /**
      * Java String Program to Check Anagram
@@ -13,6 +16,28 @@ public class Exercise7 {
      * Output: false
      */
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Input the first text : ");
+        String firstText = myScanner.nextLine();
+        System.out.print("Input the second text : ");
+        String secondText = myScanner.nextLine();
+        myScanner.close();
+        boolean isAnagram = true;
 
+        String[] firstArr = firstText.split("");
+        String[] secondtArr = secondText.split("");
+        if(firstArr.length == secondtArr.length){
+            Arrays.sort(firstArr);
+            Arrays.sort(secondtArr);
+            for(int i = 0; i < firstArr.length; i++){
+                if(!firstArr[i].equals(secondtArr[i])){
+                    isAnagram = false;
+                }
+            }
+        }
+        else{
+            isAnagram=false;
+        }
+        System.out.println("Is it anagram? : " + isAnagram);
     }
 }
