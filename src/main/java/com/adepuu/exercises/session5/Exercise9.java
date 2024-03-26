8e0923491036ecf775ae280646f3b5f5eb26ed9f
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-26 10:25:16 +0700

    feat(exercise9) done exercise 9

--- src/main/java/com/adepuu/exercises/session5/Exercise9.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise9.java
@@ -1,22 +1,35 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise9 {
-    /**
-     * Java Program to get the number of days you have to wait after the i-th day to get a warmer temperature
-     *
-     * Example 1:
-     * Input: temperatures = [73,74,75,71,69,72,76,73]
-     * Output: [1,1,4,2,1,1,0,0]
-     *
-     * Example 2:
-     * Input: temperatures = [30,40,50,60]
-     * Output: [1,1,1,0]
-     *
-     * Example 3:
-     * Input: temperatures = [30,60,90]
-     * Output: [1,1,0]
-     */
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise9 {
+    public static void main(String[] args) {
+        int[] temperature1 = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] temperature2 = {30, 40, 50, 60};
+        int[] temperature3 = {30, 60, 90};
+
+        int[] result1 = waitDays(temperature1);
+        int[] result2 = waitDays(temperature2);
+        int[] result3 = waitDays(temperature3);
+
+        System.out.println("Temperature : " + Arrays.toString(temperature1) + " Wait days : " + Arrays.toString(result1));
+        System.out.println("Temperature : " + Arrays.toString(temperature2) + " Wait days : " + Arrays.toString(result2));
+        System.out.println("Temperature : " + Arrays.toString(temperature3) + " Wait days : " + Arrays.toString(result3));
     }
+    public static int[] waitDays(int[] temperature){
+        int n = temperature.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperature[j] > temperature[i]){
+                    result[i] = j - i;
+                    break;
+                }
+                
+            }
+            
+        } return result;
+    }
+
 }
