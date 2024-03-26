76c4a5e6f2431ffb8782be8579529b8c27fdad89
nurrizkiamalia <rizkiamel9@gmail.com>
2024-03-26 14:28:27 +0700

    Feat: calculate average input number and press q to finish

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session6;
 
+import java.util.Scanner;
+
 public class Exercise1 {
     /**
      * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
@@ -13,6 +15,30 @@ public class Exercise1 {
      * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+
+        while (true){
+            String strInput = input.next();
+            if (strInput.equals("q")){
+                break;
+            }
+            try{
+                int num = Integer.parseInt(strInput);
+                sum+=num;
+                count++;
+            } catch (NumberFormatException e){
+                System.out.println("Invalid input. Please enter a valid number or 'q' to finish.");
+            }
+        }
+
+        if (count > 0){
+            double avgNumber = (double) sum / count;
+            System.out.println((int)Math.round(avgNumber));
+        } else {
+            System.out.println("invalid");
+        }
     }
 }
--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,14 +1,17 @@
 package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercise2 {
     /**
      * Complete the file reader program below
      * and handle all edge cases using try-catch-finally based on the logic given
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
@@ -16,7 +19,7 @@ public class Exercise2 {
         scanner.close();
     }
 
-    static void readFileContents(String fileName) {
+    static void readFileContents(String fileName) throws IOException {
         BufferedReader reader = null;
         reader = new BufferedReader(new FileReader(fileName));
         String line;
