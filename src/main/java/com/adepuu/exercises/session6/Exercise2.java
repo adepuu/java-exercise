ac65186296e05f1b3e4be6a83a9194d3a80feb9d
Yosef1997 <yosefsitumorang97@gmail.com>
2024-03-26 11:18:12 +0700

    sync fork

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,10 +1,13 @@
 package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercise2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
@@ -12,7 +15,7 @@ public class Exercise2 {
         scanner.close();
     }
 
-    static void readFileContents(String fileName) {
+    static void readFileContents(String fileName) throws IOException {
         BufferedReader reader = null;
         reader = new BufferedReader(new FileReader(fileName));
         String line;
