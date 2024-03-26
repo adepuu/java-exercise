58e17ed22eb87c67d6838dd62260afa896ff92f3
akaslayer <hendrysurijanto123@gmail.com>
2024-03-26 13:05:22 +0700

    Excrs1(done):Number 1 is done for try and catch

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,26 @@
 package com.adepuu.exercises.session6;
 
-public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Exercise1 {
+    public static void main(String[] args) {
+        boolean loop = false;
+        Scanner myInput = new Scanner(System.in);
+        int sum = 0;
+        while(!loop) {
+        System.out.print("Input = ");
+        String input = myInput.next();
+        if(input.equals("q")){
+            loop = true;
+        }else{
+            try{
+                sum += Integer.parseInt(input);
+            } catch (Exception e)
+            {
+                System.out.println("Invalid input. Please enter a valid number or 'q' to finish.");
+            }
+            }
+        }
+        System.out.println("Total = " + sum);
     }
 }
