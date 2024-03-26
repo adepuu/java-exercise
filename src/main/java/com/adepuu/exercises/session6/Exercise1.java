9c6434571b88648ede5ad905d5b09081d114b493
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-26 14:18:59 +0700

    feat(Exercise1 and Exercise2): completed

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session6;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise1 {
     /**
      * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
@@ -13,6 +17,33 @@ public class Exercise1 {
      * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
      */
     public static void main(String[] args) {
+        calcAvg();
+    }
 
+    public static void calcAvg() {
+        Scanner input = new Scanner(System.in);
+
+        int sum = 0;
+        List<Integer> nums = new ArrayList<>();
+
+        try {
+            while (true) {
+                System.out.print("Enter your number or press 'q' to finish: ");
+                String userInput = input.next();
+                if (userInput.equals("q")) {
+                    break;
+                }
+                int num = Integer.parseInt(userInput);
+                nums.add(num);
+            }
+
+            for (int num: nums) {
+                sum += num;
+            }
+            System.out.println("This is your inputted number: " + nums);
+            System.out.println("and the average is: " + (sum / nums.size()));
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a valid number or 'q' to finish.");
+        }
     }
 }
--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,23 +1,44 @@
 package com.adepuu.exercises.session6;
 
+import com.adepuu.Main;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+        String currentDir = userDir + "\\src\\main\\java\\com\\adepuu\\exercises\\session6\\";
         Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter folder name: ");
+//        String folderName = scanner.nextLine();
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
-        readFileContents(fileName);
+        readFileContents(currentDir + fileName);
         scanner.close();
     }
 
     static void readFileContents(String fileName) {
         BufferedReader reader = null;
-        reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error occurred when reading the file: " + e.getMessage());
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    System.out.println("Error occurred when reading the file: " + e.getMessage());
+                }
+            }
         }
     }
 }
