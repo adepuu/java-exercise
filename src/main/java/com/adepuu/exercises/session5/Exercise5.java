e1489df3c998b406a8d2b1dbe3740a32447c4d99
Jacob Irwanto <jacobirwanto@gmail.com>
2024-03-26 09:57:33 +0700

    feat(exercise5) done exercise 5

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,18 +1,38 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise5 {
-    /**
-     * Java Array Program to sort array in increasing & decreasing order
-     *
-     * Example 1:
-     * Input: [ 8, 7, 5, 2], direction = “asc”
-     * Output: [ 2, 5, 7, 8 ]
-     *
-     * Example 2:
-     * Input: [ 8, 7, 5, 2], direction = “desc”
-     * Output: [ 8,7, 5, 2 ]
-     */
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Exercise5 {
+    public static void main(String[] args) {
+        int[] array1 = {8, 7, 5, 2};
+        String direction1 = "asc";
+        sortArray(array1, direction1);
+        System.out.println(Arrays.toString(array1));
+
+        int[] array2 = {8, 7, 5, 2};
+        String direction2= "desc";
+        sortArray(array2, direction2);
+        System.out.println(Arrays.toString(array2));
     }
+    public static void sortArray(int[] arr, String direction){
+        if (direction.equals("asc")){
+            Arrays.sort(arr);
+        } else if (direction.equals("desc")) {
+            Arrays.sort(arr);
+            reverse(arr);
+        }
+    }
+
+    public static void reverse(int[] arr){
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
 }
