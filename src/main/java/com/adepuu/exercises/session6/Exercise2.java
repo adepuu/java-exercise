d6b2e445d1982c5a34319162269553ee91139c1d
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-03-26 14:37:52 +0700

    revised(Exercise2) manual input user path

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,23 +1,16 @@
 package com.adepuu.exercises.session6;
 
-import com.adepuu.Main;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        String userDir = System.getProperty("user.dir");
-        String currentDir = userDir + "\\src\\main\\java\\com\\adepuu\\exercises\\session6\\";
         Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter folder name: ");
-//        String folderName = scanner.nextLine();
         System.out.print("Enter the file name: ");
         String fileName = scanner.nextLine();
-        readFileContents(currentDir + fileName);
+        readFileContents(fileName);
         scanner.close();
     }
 
