475a9ebb04938bc5da944fa58497c8af5ff65c20
Christofer Bryan <christoferbryan45@gmail.com>
2024-03-26 10:37:46 +0800

    feat(exercise): completed exercise 4

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Exercise4 {
     /**
      * Java Array Program to Remove Duplicate Elements From an Array
@@ -9,6 +12,21 @@ public class Exercise4 {
      * @param args
      */
     public static void main(String[] args) {
+        int[] input = {1, 2, 2, 3, 3, 3, 4, 5};
+        System.out.println(removeDuplicates(input));
+    }
 
+    public static ArrayList<Integer> removeDuplicates(int[] inputArray){
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < inputArray.length; i++){
+            if (map.get(inputArray[i]) == null) {
+                result.add(inputArray[i]);
+                map.put(inputArray[i], true);
+            }
+        }
+
+        return result;
     }
 }
