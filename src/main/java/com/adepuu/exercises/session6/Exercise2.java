43f415401edcb2aa5631f09736bff98ab2aedebc
irwinekabakti <irwinekabakti@gmail.com>
2024-03-26 13:15:38 +0700

    add:(exercise2-session6)

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,27 +1,44 @@
 package com.adepuu.exercises.session6;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.util.Scanner;
+import java.io.*;
 
 public class Exercise2 {
     /**
      * Complete the file reader program below
      * and handle all edge cases using try-catch-finally based on the logic given
      */
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter the file name: ");
-//        String fileName = scanner.nextLine();
-//        readFileContents(fileName);
-//        scanner.close();
-//    }
-//
-//    static void readFileContents(String fileName) {
-//        BufferedReader reader = null;
-//        reader = new BufferedReader(new FileReader(fileName));
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            System.out.println(line);
-//        }
-//    }
+
+
+    public static void readFileContents(String fileName, Scanner scanner) {
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.print("Sorry, file not found : " + e.getMessage());
+        } catch (IOException e) {
+            System.out.print("Sorry, there is an error at the file : " + e.getMessage());
+        } finally {
+            System.out.print("Do you want to open another file? (Press 'y' to continue) : ");
+            if(scanner.nextLine().equals("y")) {
+                handleFile(scanner);
+            }
+        }
+    }
+    public static void handleFile (Scanner scanner){
+        System.out.print("Enter the file name : ");
+        String fileName = scanner.nextLine();
+        readFileContents(fileName, scanner);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        handleFile(scanner);
+        scanner.close();
+    }
 }
