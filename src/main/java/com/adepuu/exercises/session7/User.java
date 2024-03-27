3a6d0f562a62ab222d4d890b567fd3a261c4038e
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-03-27 16:21:32 +0700

    feat(User): add new class User

--- src/main/java/com/adepuu/exercises/session7/User.java
+++ src/main/java/com/adepuu/exercises/session7/User.java
@@ -1,2 +1,54 @@
-package com.adepuu.exercises.session7;public class User {
+package com.adepuu.exercises.session7;
+
+class User {
+    private String name;
+    private String email;
+    private String ticketId;
+
+
+
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name){
+        if(!name.isEmpty()){
+            this.name = name;
+        }
+        return;
+    }
+
+    public void setEmail(String email){
+        if(!email.isEmpty()){
+            this.email = email;
+        }
+        return;
+    }
+
+    public void setTicketId(String ticketId) {
+        if (!ticketId.isEmpty()){
+            this.ticketId = ticketId;
+        }
+        return;
+    }
+
+    public void printUserDetails(){
+        System.out.println("Name : " + name);
+        System.out.println("Email : " + email);
+        System.out.println("Ticket ID : " + ticketId);
+    }
 }
