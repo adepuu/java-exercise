599195e57b2483c7f9f8486a289a24e4b3dd3e3d
Jacob Irwanto <jacobirwanto@gmail.com>
2024-04-02 08:54:24 +0700

    feat(session10) done session 10 exercise

--- src/main/java/com/adepuu/exercises/session8/Auth.java
+++ src/main/java/com/adepuu/exercises/session8/Auth.java
@@ -1,60 +1,60 @@
-package com.adepuu.exercises.session8;
-
-import java.util.HashMap;
-
-public class Auth {
-    private HashMap<String, User> registeredUsers = new HashMap<>();
-    private String activeUserName;
-
-    public boolean registerUser(User newUser) {
-        if (registeredUsers.containsKey(newUser.getUsername())) {
-            System.out.println("Username already exist, choose other username!");
-            return false;
-        }
-        registeredUsers.put(newUser.getUsername(), newUser);
-        activeUserName = newUser.getUsername();
-        return true;
-    }
-
-    public String getActiveUserID() {
-        return activeUserName;
-    }
-
-    public boolean logout() {
-        if (this.activeUserName.isEmpty()) {
-            System.out.println("Session empty");
-            return false;
-        }
-
-        this.activeUserName = "";
-        System.out.println("Logged Out");
-        return true;
-    }
-
-    public boolean login(String username, String password) {
-        if (username.isEmpty() || password.isEmpty()) return false;
-
-        if (!registeredUsers.containsKey(username)) {
-            User newUser = new User(username, password);
-            if (registerUser(newUser)) {
-                System.out.println("New user registered, logged in");
-                this.activeUserName = newUser.getUsername();
-                return true;
-            }
-            System.out.println("Something wrong :(");
-            return false;
-        }
-        var currentUser = registeredUsers.get(username);
-        if (currentUser.checkCredentials(username, password)) {
-            System.out.println("Logged in, welcome " + currentUser.getUsername());
-            this.activeUserName = currentUser.getUsername();
-            return true;
-        }
-        System.out.println("Wrong credentials!");
-        return false;
-    }
-
-    public User getCurrentUserDetails() {
-        return registeredUsers.get(activeUserName);
-    }
-}
+//package com.adepuu.exercises.session8;
+//
+//import java.util.HashMap;
+//
+//public class Auth {
+//    private HashMap<String, User> registeredUsers = new HashMap<>();
+//    private String activeUserName;
+//
+//    public boolean registerUser(User newUser) {
+//        if (registeredUsers.containsKey(newUser.getUsername())) {
+//            System.out.println("Username already exist, choose other username!");
+//            return false;
+//        }
+//        registeredUsers.put(newUser.getUsername(), newUser);
+//        activeUserName = newUser.getUsername();
+//        return true;
+//    }
+//
+//    public String getActiveUserID() {
+//        return activeUserName;
+//    }
+//
+//    public boolean logout() {
+//        if (this.activeUserName.isEmpty()) {
+//            System.out.println("Session empty");
+//            return false;
+//        }
+//
+//        this.activeUserName = "";
+//        System.out.println("Logged Out");
+//        return true;
+//    }
+//
+//    public boolean login(String username, String password) {
+//        if (username.isEmpty() || password.isEmpty()) return false;
+//
+//        if (!registeredUsers.containsKey(username)) {
+//            User newUser = new User(username, password);
+//            if (registerUser(newUser)) {
+//                System.out.println("New user registered, logged in");
+//                this.activeUserName = newUser.getUsername();
+//                return true;
+//            }
+//            System.out.println("Something wrong :(");
+//            return false;
+//        }
+//        var currentUser = registeredUsers.get(username);
+//        if (currentUser.checkCredentials(username, password)) {
+//            System.out.println("Logged in, welcome " + currentUser.getUsername());
+//            this.activeUserName = currentUser.getUsername();
+//            return true;
+//        }
+//        System.out.println("Wrong credentials!");
+//        return false;
+//    }
+//
+//    public User getCurrentUserDetails() {
+//        return registeredUsers.get(activeUserName);
+//    }
+//}
