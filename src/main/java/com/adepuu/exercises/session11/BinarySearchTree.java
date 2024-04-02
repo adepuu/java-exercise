abf3f1c2164007b9ab2095dd5ef9f9e8b4db9212
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-04-02 16:34:57 +0700

    feat(exercise11) : add new feature binaryTreeSearch, Stack, Queue

--- src/main/java/com/adepuu/exercises/session11/BinarySearchTree.java
+++ src/main/java/com/adepuu/exercises/session11/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session11;
 
+import com.adepuu.dsa.datastructure.BinaryTree.BinaryTree;
+
+import java.util.Scanner;
+
 public class BinarySearchTree {
     /**
      * Write a Java binary search tree program that can scale dynamically
@@ -20,6 +24,26 @@ public class BinarySearchTree {
      *
      */
     public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
+            System.out.print("Enter the number (or 'q' to quit): ");
+            input = scanner.nextLine();
 
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+            int num = Integer.parseInt(input);
+            bt.insert(num);
+        } while (true);
+
+        System.out.print("Enter a number to search for: ");
+        int searchNum = Integer.parseInt(scanner.nextLine());
+
+        boolean isExist = bt.search(searchNum);
+        System.out.println("Number " + searchNum + " is " + (isExist ? "found" : "not found") + " in the binary tree.");
+
+        scanner.close();
     }
 }
--- src/main/java/com/adepuu/exercises/session11/Stack.java
+++ src/main/java/com/adepuu/exercises/session11/Stack.java
@@ -1,6 +1,8 @@
 package com.adepuu.exercises.session11;
 
-public class Stack {
+import java.util.LinkedList;
+
+public class Stack<T> {
     /**
      * Write a Java stack program that can scale dynamically
      * <p>
@@ -15,7 +17,58 @@ public class Stack {
      * - Memory Efficiency: The stack should use memory efficiently, allocating and deallocating memory dynamically as needed.
      * - Error Handling: The program should handle edge cases gracefully, such as attempting to pop an element from an empty stack, and provide clear error messages.
      */
-    public static void main(String[] args) {
-
+    private LinkedList<T> stackList;
+    Stack(){
+        stackList = new LinkedList<>();
     }
+
+    public boolean isEmpty(){
+        return stackList.isEmpty();
+    }
+    public void push(T value){
+        stackList.addLast(value);
+    }
+    public void pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty. Cannot pop.");
+        } else {
+             stackList.removeLast();
+        }
+    }
+    public T peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty. Cannot pop.");
+            return null;
+        } else {
+            return stackList.getLast();
+        }
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        System.out.println("1."+ stack.peek());
+        stack.push(2);
+        System.out.println("2."+ stack.peek());
+        stack.push(3);
+        System.out.println("3."+ stack.peek());
+        stack.push(4);
+        System.out.println("4."+ stack.peek());
+        stack.push(5);
+        System.out.println("5."+ stack.peek());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        System.out.println("6."+ stack.peek());
+
+
+
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.peek());
+    }
+
+
+
+
+
 }
